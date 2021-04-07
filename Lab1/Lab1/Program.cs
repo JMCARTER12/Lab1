8cfc5068424e6ec9250a850c6d1335a41e0e2dda
JMCARTER12 <82064669+JMCARTER12@users.noreply.github.com>
2021-04-07 15:49:22 -0500

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Grading ID: R8206, Lab Number: 01, Due Date:Jan 19, 2020, Section Number: 01,This program is written to show different things about myself\\
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Grading ID:     R8206");
+            System.Console.WriteLine("Hobbies:        Exercising and Making Beats");
+            System.Console.WriteLine("Favorite Book:  The Hunger Games");
+            System.Console.WriteLine("Favorite Movie: Django Unchained");
+        }
+    }
+}
